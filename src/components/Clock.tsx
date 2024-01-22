e10d5927aa3b89149e7a34467cccc686dcf19c87
melonMochiRoll <soulhwasin@gmail.com>
2024-01-23 00:52:57 +0900

    :lipstick: Display hours, minutes, seconds

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -14,11 +14,20 @@ const Clock: FC<ClockProps> = ({
 }) => {
   return (
     <Block>
-      <span>{hour}</span>
-      <span>:</span>
-      <span>{minute}</span>
-      <span>:</span>
-      <span>{second}</span>
+      <Box>
+        <Display>{hour}</Display>
+        <HMS>HOURS</HMS>
+      </Box>
+      <Display>:</Display>
+      <Box>
+        <Display>{minute}</Display>
+        <HMS>MINUTES</HMS>
+      </Box>
+      <Display>:</Display>
+      <Box>
+        <Display>{second}</Display>
+        <HMS>SECONDS</HMS>
+      </Box>
     </Block>
   );
 };
@@ -27,11 +36,26 @@ export default Clock;
 
 const Block = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   color: #fff;
+  gap: 10px;
+`;
+
+const Box = styled.div`
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Display = styled.span`
   font-size: 150px;
   font-weight: 800;
-  gap: 10px;
   text-shadow: 0 0 6px #fff;
+`;
+
+const HMS = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+  text-shadow: 0 0 3px #fff;
+  text-align: center;
 `;
