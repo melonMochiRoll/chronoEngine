92e26971b61bd7e8b6cb734523b846e37f81eed8
melonMochiRoll <soulhwasin@gmail.com>
2024-01-25 22:29:47 +0900

    :lipstick: Add style width: 100%

--- src/containers/CurrentTimeContainer.tsx
+++ src/containers/CurrentTimeContainer.tsx
@@ -54,7 +54,7 @@ const Block = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 100vw;
+  width: 100%;
   min-height: 100vh;
   background-image:
     url('https://images.unsplash.com/photo-1439792675105-701e6a4ab6f0?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1473w');
