23a2f6e52933993880dae110f0232b31c6dcb4fb
melonMochiRoll <soulhwasin@gmail.com>
2024-01-28 17:55:44 +0900

    :sparkles: Import RenerModal

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react';
 import styled from '@emotion/styled';
 import CurrentTimeContainer from 'Containers/CurrentTimeContainer';
+import TimerContainer from 'Containers/TimerContainer';
+import RenderModal from 'Components/common/RenderModal';
 
 const MainPage: FC = () => {
   return (
     <Block>
+      <RenderModal />
       <CurrentTimeContainer />
+      <TimerContainer />
     </Block>
   );
 };
@@ -14,11 +18,6 @@ export default MainPage;
 
 const Block = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  min-height: 100vh;
-  background-image:
-    url('https://images.unsplash.com/photo-1439792675105-701e6a4ab6f0?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1473w');
-  background-size: cover;
-  background-position-y: center;
 `;
