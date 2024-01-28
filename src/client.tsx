e7db14925718010dfca4dedc1067dfca7f8c5ffa
melonMochiRoll <soulhwasin@gmail.com>
2024-01-28 17:45:38 +0900

    :art: Set redux provider

--- src/client.tsx
+++ src/client.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import MainRouter from 'Routes/MainRouter';
 import { GlobalStyle } from 'Styles/GlobalStyle';
+import reduxStore from './store';
 
 const rootNode = document.getElementById('root') as HTMLElement;
 
 createRoot(rootNode).render(
   <React.StrictMode>
     <GlobalStyle />
-    <RouterProvider router={MainRouter} />
+    <Provider store={reduxStore}>
+      <RouterProvider router={MainRouter} />
+    </Provider> 
   </React.StrictMode>
 );
