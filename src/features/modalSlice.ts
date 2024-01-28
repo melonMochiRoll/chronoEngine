6e4d41d8f608f67d054779429e84ed489a1828fa
melonMochiRoll <soulhwasin@gmail.com>
2024-01-28 17:43:44 +0900

    :art: Remove initialState in isOpen

--- src/features/modalSlice.ts
+++ src/features/modalSlice.ts
@@ -4,15 +4,13 @@ export const modalSlice = createSlice({
   name: 'modal',
   initialState: {
     name: '',
-    isOpen: false,
   },
   reducers: {
     openModal: (state, action) => {
       state.name = action.payload.name;
-      state.isOpen = true;
     },
     closeModal: (state) => {
-      state.isOpen = false;
+      state.name = '';
     },
   },
 });
