21a5dc66d09f2259886c36892662b3ce5b468b5a
melonMochiRoll <soulhwasin@gmail.com>
2024-01-30 11:39:59 +0900

    :sparkles: Add title

--- src/components/InputClock.tsx
+++ src/components/InputClock.tsx
@@ -3,49 +3,68 @@ import styled from '@emotion/styled';
 import { HMS } from 'Hooks/useTimer';
 
 interface InputClockProps {
+  title: string;
   value: HMS;
   onChange: (e: any) => void;
 };
 
 const InputClock: FC<InputClockProps> = ({
+  title,
   value,
   onChange,
 }) => {
   return (
-    <Top>
-      <Input
-        type='number'
-        name='hour'
-        value={value.hour}
-        onChange={onChange}
-        min={0}
-        max={99}
-        placeholder='00' />
-      <span>:</span>
-      <Input
-        type='number'
-        name='minute'
-        value={value.minute}
-        onChange={onChange}
-        min={0}
-        max={59}
-        placeholder='00' />
-      <span>:</span>
-      <Input
-        type='number'
-        name='second'
-        value={value.second}
-        onChange={onChange}
-        min={0}
-        max={59}
-        placeholder='00' />
-    </Top>
+    <Block>
+      <Title>{title}</Title>
+      <InputBox>
+        <Input
+          type='number'
+          name='hour'
+          value={value.hour}
+          onChange={onChange}
+          min={0}
+          max={99}
+          placeholder='00' />
+        <span>:</span>
+        <Input
+          type='number'
+          name='minute'
+          value={value.minute}
+          onChange={onChange}
+          min={0}
+          max={59}
+          placeholder='00' />
+        <span>:</span>
+        <Input
+          type='number'
+          name='second'
+          value={value.second}
+          onChange={onChange}
+          min={0}
+          max={59}
+          placeholder='00' />
+      </InputBox>
+    </Block>
   );
 };
 
 export default InputClock;
 
-const Top = styled.div`
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.span`
+  color: #fff;
+  font-size: 32px;
+  text-shadow: 0 0 3px #fff;
+  padding-bottom: 20px;
+`;
+
+const InputBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
