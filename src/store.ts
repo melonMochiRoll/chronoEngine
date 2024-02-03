35e25caaa96ef1d4a6d71bbf5191612e0750a533
melonMochiRoll <soulhwasin@gmail.com>
2024-02-03 17:17:48 +0900

    :heavy_plus_sign: Import modeReducer

--- src/store.ts
+++ src/store.ts
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from 'Features/modalSlice';
 import timerReducer from 'Features/timerSlice';
 import cycleTimerReducer from "Features/cycleTimerSlice";
+import modeReducer from "Features/modeSlice";
 
 const reduxStore = configureStore({
   reducer: {
+    mode: modeReducer,
     modal: modalReducer,
     timer: timerReducer,
     cycleTimer: cycleTimerReducer,
