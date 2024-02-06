e1226b6de4266d214032a6505aff7783320fa53a
melonMochiRoll <soulhwasin@gmail.com>
2024-02-06 16:08:28 +0900

    :sparkles: Add store dispatch

--- src/client.tsx
+++ src/client.tsx
@@ -5,8 +5,11 @@ import { RouterProvider } from 'react-router-dom';
 import MainRouter from 'Routes/MainRouter';
 import { GlobalStyle } from 'Styles/GlobalStyle';
 import reduxStore from './store';
+import { fetchRecords } from 'Features/recordSlice';
 
 const rootNode = document.getElementById('root') as HTMLElement;
+
+reduxStore.dispatch(fetchRecords);
 
 createRoot(rootNode).render(
   <React.StrictMode>
