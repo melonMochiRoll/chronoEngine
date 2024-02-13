8333fe77beda7b33795cd366e3832168c9401b80
melonMochiRoll <soulhwasin@gmail.com>
2024-02-13 17:17:04 +0900

    :art: Add recordModalOpen and Refactor code

--- src/containers/CycleTimerContainer.tsx
+++ src/containers/CycleTimerContainer.tsx
@@ -2,62 +2,68 @@ import React, { FC, useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import TimeFormat from 'Components/TimeFormat';
 import { toHMS } from 'Utils/time';
-import { ModalName } from 'Components/common/RenderModal';
-import { useAppDispatch, useAppSelector } from 'Hooks/reduxHooks';
+import { ModalCode } from 'Components/common/RenderModal';
 import { openModal } from 'Features/modalSlice';
 import CycleProgressCircle from 'Components/CycleProgressCircle';
+import CycleTimerDisplay from 'Components/CycleTimerDisplay';
+import CycleTimerButtonBox from 'Components/CycleTimerButtonBox';
+import { useAppDispatch } from 'Hooks/reduxHooks';
+import { ITimeFormat } from 'Features/cycleTimerSlice';
 
 interface CycleTimerContainerProps {
-  cycleTimer: any;
+  currentTime: ITimeFormat;
+  cycleCount: number;
   running: boolean;
+  progress: number;
   onSubmit: () => void;
 };
 
 const CycleTimerContainer: FC<CycleTimerContainerProps> = ({
-  cycleTimer,
+  currentTime,
+  cycleCount,
   running,
+  progress,
   onSubmit,
 }) => {
   const dispatch = useAppDispatch();
   const [ HMS, setHMS ] = useState({ hour: 0, minute: 0, second: 0 });
-  const { originTime, workTime, restTime, currentTime } = cycleTimer;
 
   useEffect(() => {
-    setHMS(toHMS(currentTime));
+    setHMS(toHMS(currentTime.current));
   }, [currentTime]);
 
-  const onOpen = () => {
+  const timerModalOpen = () => {
     if (!running) {
       dispatch(openModal({
-        name: ModalName.CycleTimerModal,
+        code: ModalCode.CycleTimerModal,
       }));
     };
+  };
+
+  const recordModalOpen = () => {
+    dispatch(openModal({
+      code: ModalCode.RecordModal,
+    }));
   };
 
   return (
     <Block>
       <CycleProgressCircle
-        originTime={originTime}
-        workTime={workTime}
-        restTime={restTime}
-        currentTime={currentTime} />
+        progress={progress}
+        mode={currentTime.mode} />
       <Content>
-        {
-          originTime.length ?
-            <Mode>Work</Mode> :
-            <Mode>Rest</Mode>
-        }
+        <CycleTimerDisplay
+          onClick={() => recordModalOpen()}
+          mode={currentTime.mode}
+          cycleCount={cycleCount} />
         <TimeFormat
-          onClick={() => onOpen()}
+          onClick={() => timerModalOpen()}
           hour={`${HMS.hour}`}
           minute={`${HMS.minute}`}
           second={`${HMS.second}`} />
-        <ControlBox>
-          <Button
-            onClick={() => onSubmit()}>
-            {running ? '중지' : '시작'}
-          </Button>
-        </ControlBox>
+        <CycleTimerButtonBox
+          running={running}
+          onSubmit={onSubmit} />
       </Content>
     </Block>
   );
@@ -81,34 +87,4 @@ const Content = styled.div`
   flex-direction: column;
   align-items: center;
   z-index: 1;
-`;
-
-const Mode = styled.span`
-  color: #fff;
-  font-size: 64px;
-  text-shadow: 0 0 3px #fff;
-  padding-bottom: 90px;
-`;
-
-const ControlBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 80px;
-`;
-
-const Button = styled.button`
-  color: #fff;
-  font-size: 26px;
-  font-weight: 600;
-  text-shadow: 0 0 3px #fff;
-  border: 1px solid rgba(0, 0, 0, 0);
-  border-radius: 25px;
-  padding: 4px 18px;
-  background-color: rgba(0, 0, 0, 0);
-  cursor: pointer;
-  transition: all 0.3s;
-
-  &:hover {
-    border: 1px solid #fff;
-  }
 `;
