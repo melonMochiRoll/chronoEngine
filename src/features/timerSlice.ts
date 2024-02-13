80b7f3cfd152c4bfc3a2b518d837e06374834f34
melonMochiRoll <soulhwasin@gmail.com>
2024-02-13 17:39:03 +0900

    :label: Add types

--- src/features/timerSlice.ts
+++ src/features/timerSlice.ts
@@ -1,18 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+export const getProgress = (timer: ITimerInitState) => {
+  return 100 - Math.floor(timer.currentTime/timer.originTime * 100)
+};
+
+interface ITimerInitState {
+  originTime: number,
+  currentTime: number,
+};
+
+const initialState: ITimerInitState = {
+  originTime: 0,
+  currentTime: 0,
+};
 
 export const timerSlice = createSlice({
   name: 'timer',
-  initialState: {
-    originTime: 0,
-    currentTime: 0,
-  },
+  initialState: initialState,
   reducers: {
-    initTime: (state, action) => {
+    initTime: (state, action: PayloadAction<{ time: number }>) => {
       state.originTime = action.payload.time;
       state.currentTime = action.payload.time;
     },
-    setTime: (state, action) => {
-      state.currentTime = action.payload.time;
+    setTime: (state, action: PayloadAction<{ current: number }>) => {
+      state.currentTime = action.payload.current;
     },
     resetTime: (state) => {
       state.currentTime = state.originTime;
