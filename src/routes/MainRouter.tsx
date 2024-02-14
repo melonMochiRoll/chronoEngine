da34aaa728dde2826da6c31131b0b0d61b9f1f60
melonMochiRoll <soulhwasin@gmail.com>
2024-02-14 14:50:05 +0900

    :heavy_plus_sign: Import NotFoundPage

--- src/routes/MainRouter.tsx
+++ src/routes/MainRouter.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from 'Pages/MainPage';
+import NotFoundPage from 'Pages/NotFoundPage';
 
 const MainRouter = createBrowserRouter([
   {
     path: '/',
     element: <MainPage />,
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />
   },
 ]);
 
