bb8d5d58983c705f136284d50c437d501e72f052
melonMochiRoll <soulhwasin@gmail.com>
2024-02-21 18:21:32 +0900

    :bulb: Add comments to toHMS

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param second 2 = 2 second, 60 = 1 minute
+ * @returns // return { hour, minute, second }
+ */
+
 export const toHMS = (second: number) => {
   const hour = Math.floor(second / 3600);
   second = Math.floor(second % 3600);
